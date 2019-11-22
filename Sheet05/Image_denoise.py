f226a285fae299f75552bfd33546780c47b8bc22
birdy666 <allenchang1227@gmail.com>
2019-11-23 00:57:42 +0100

    Task4 finished

--- Sheet05/Image_denoise.py
+++ Sheet05/Image_denoise.py
@@ -37,22 +37,93 @@ def question_4(I,rho=0.6):
     ### Use Alpha expansion binary image for each label
 
     ### 1) Define Graph
-
+    def getGraph(img):                
+        g = maxflow.Graph[float]()
+        return g
+        
     ### 2) Add pixels as nodes
-
+    def getNodes(img, g):
+        row =img.shape[0]
+        col = img.shape[1]
+        nodes = g.add_nodes(row*col)
+        return nodes
     ### 3) Compute Unary cost
-
     ### 4) Add terminal edges
+    def addTedges(g,  nodes, a, img):
+        row =img.shape[0]
+        col = img.shape[1]
+
+        for i in range(row):
+            for j in range(col):
+                if img[i][j] == a:
+                    g.add_tedge(nodes[i*col + j], 0.8, float("inf"))
+                else:
+                    g.add_tedge(nodes[i*col + j], 0.8, 0.1)                
+        return g
 
     ### 5) Add Node edges
-    ### Vertical Edges
+    def Potts(A, B):
+            return(0 if A == B else 1) 
 
+    def addEdges(g, nodes, a, img): # need row/col because nodes is 1D
+        g1 =addTedges(g,  nodes, a, img)
+        g2 = addVEdges(g1,  nodes, a, img)
+        g3 = addHEdges(g2,  nodes, a, img)
+        return g3
+    ### Vertical Edges
+    def addHEdges(g,  nodes, a, img):
+        row =img.shape[0]
+        col = img.shape[1]
+
+        for i in range(row):
+            for j in range(col-1):
+                P = Potts(img[i][j], img[i][j+1])
+                g.add_edge(nodes[i*col + j], nodes[i*col + j + 1], P, P)
+        return g
     ### Horizontal edges
     # (Keep in mind the stucture of neighbourhood and set the weights according to the pairwise potential)
+    def addVEdges(g,  nodes, a, img):
+        row =img.shape[0]
+        col = img.shape[1]
+
+        for j in range(col):
+            for i in range(row-1):
+                P = Potts(img[i][j], img[i+1][j])
+                g.add_edge(nodes[i*col + j], nodes[(i+1)*col + j ], P, P)
+        return g
+
+    def updateImg(g, img, nodes, a):
+        newImg = img.copy()
+        row =img.shape[0]
+        col = img.shape[1]
+        for i in range(row):
+            for j in range(col):
+                if g.get_segment(nodes[i*col+j]) == 1:
+                    newImg[i][j] = a
+        return newImg
 
     ### 6) Maxflow
+    img = I
+    flows = [0,0,0]
+    temp = [0,0,0]
+
+    while True:
+        for i in range(len(labels)):
+            g = getGraph(img)
+            nodes = getNodes(img, g)
+            g = addEdges(g, nodes, labels[i], img)
+            flows[i] = g.maxflow()
+            img = updateImg(g, img, nodes, labels[i])
+            
+        if flows == temp:
+            break
+        else: # yes, idk how to do deep copy :)
+            temp[0] = flows[0]
+            temp[1] = flows[1]
+            temp[2] = flows[2]
 
 
+    Denoised_I = img
     cv2.imshow('Original Img', I), \
     cv2.imshow('Denoised Img', Denoised_I), cv2.waitKey(0), cv2.destroyAllWindows()
 
@@ -68,7 +139,7 @@ def main():
     question_3(image_q3, rho=0.7, pairwise_cost_same=0.005, pairwise_cost_diff=0.55)
 
     ### Call solution for question 4
-    question_4(image_q4, rho=0.8)
+    question_4(image_q4, rho=0.8)    
     return
 
 if __name__ == "__main__":
