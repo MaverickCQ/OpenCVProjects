f5597651585fa6d4ca14d148f0de1e9cb3e2e50e
birdy666 <allenchang1227@gmail.com>
2019-12-14 23:33:54 +0100

    task3 first edition

--- sheet08/exercise3.py
+++ sheet08/exercise3.py
@@ -1,7 +1,12 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pylab as plt
+from sklearn.neighbors import NearestNeighbors
 
+def showImg(img, name="Image"):
+    cv.imshow(name,img) 
+    cv.waitKey(0) 
+    cv.destroyAllWindows()
 
 def main():
     # Load the images
@@ -17,11 +22,38 @@ def main():
     #print(descriptors1)
     keypoints2 , descriptors2 = sift.detectAndCompute(img2, None)
     
+
+    KPImg_1=cv.drawKeypoints(img1.copy(),keypoints1,img1.copy())
+    KPImg_2=cv.drawKeypoints(img2.copy(),keypoints2,img2.copy())
+    
+    cv.imwrite("data/exercise3/keyPoints_1.png", KPImg_1)
+    cv.imwrite("data/exercise3/keyPoints_2.png", KPImg_2)
     # your own implementation of matching
+    """print(descriptors2)"""
+    neigh = NearestNeighbors(2, 0.4)
+    neigh.fit(descriptors2)   
+
+    newImg = img1.copy()
+    draw_params = dict(matchColor = (0,255,0))
+    matches = []
+    for i in range(len(keypoints1)):
+        distance, index = neigh.kneighbors([descriptors1[i]], 2, return_distance=True)
+        ratio = distance[0][0] / distance[0][1]
+        # accept the first match
+        if ratio < 0.4:
+            match = cv.DMatch(i, index[0][0], distance[0][0])
+            matches.append(match)
+            print(i)
+            
+
+        """print(ratio)"""
 
     # display the matches
-
-    pass
+    """showImg(newImg)"""
+    newImg = cv.drawMatches(newImg,keypoints1,img2,keypoints2,matches, None, **draw_params)
+    showImg(newImg)
+    cv.imwrite("data/exercise3/mountainMatches.png", newImg)
+    
 
 
 if __name__ == '__main__':
