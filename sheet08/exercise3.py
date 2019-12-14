a943e44afa67efbc421d0b55c15b4fb9d0a33f23
unknown <s6anghos@uni-bonn.de>
2019-12-14 20:52:40 +0100

    ex3

--- sheet08/exercise3.py
+++ sheet08/exercise3.py
@@ -1,13 +1,22 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import matplotlib.pylab as plt
 
 
 def main():
     # Load the images
+    img1 = cv.imread("data/exercise3/mountain1.png")
+    img2 = cv.imread("data/exercise3/mountain2.png")
+    #print(img1.shape)
+    #print(img2.shape)
 
     # extract sift keypoints and descriptors
-
+    sift = cv.xfeatures2d.SIFT_create()
+    keypoints1 , descriptors1 = sift.detectAndCompute(img1, None)
+    #print(keypoints1)
+    #print(descriptors1)
+    keypoints2 , descriptors2 = sift.detectAndCompute(img2, None)
+    
     # your own implementation of matching
 
     # display the matches
