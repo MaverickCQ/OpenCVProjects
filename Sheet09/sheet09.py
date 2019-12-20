8e29eac8ceca35dfc2e6862bd2009c9ae9272329
birdy666 <allenchang1227@gmail.com>
2019-12-21 00:36:32 +0100

    HS first edition

--- Sheet09/sheet09.py
+++ Sheet09/sheet09.py
@@ -5,11 +5,12 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 
 
+
 def load_FLO_file(filename):
     assert os.path.isfile(filename), 'file does not exist: ' + filename   
     flo_file = open(filename,'rb')
     magic = np.fromfile(flo_file, np.float32, count=1)
-    assert magic == 202021.25,  'Magic number incorrect. .flo file is invalid'
+    assert magic == 202021.25,  'Magic number incorrect. .flo file is invalid';
     w = np.fromfile(flo_file, np.int32, count=1)
     h = np.fromfile(flo_file, np.int32, count=1)
     data = np.fromfile(flo_file, np.float32, count=2*w[0]*h[0])
@@ -25,7 +26,7 @@ class OpticalFlow:
 
         # Parameters for Horn_Schunck_flow()
         self.EPSILON= 0.002 # the stopping criterion for the difference when performing the Horn-Schuck algorithm
-        self.MAX_ITERS = 1000 # maximum number of iterations allowed until convergence of the Horn-Schuck algorithm
+        self.MAX_ITERS = 200 # maximum number of iterations allowed until convergence of the Horn-Schuck algorithm
         self.ALPHA = 1.0 # smoothness term
 
         # Parameter for flow_map_to_bgr()
@@ -54,9 +55,9 @@ class OpticalFlow:
     #***********************************************************************************
     # function for converting flow map to to BGR image for visualisation
     # return bgr image
-    def flow_map_to_bgr(self, flow):
+    def flow_map_to_bgr(self, flow, step=7):
         flow_bgr = None
-
+        
         return flow_bgr
 
     #***********************************************************************************
@@ -73,9 +74,29 @@ class OpticalFlow:
     # returns the Optical flow based on the Horn-Schunck algorithm and visualisation result
     def Horn_Schunck_flow(self):
         flow = None
+        LapKernel = np.array([[0, 1/4, 0],
+                  [1/4,    -1, 1/4],
+                  [0, 1/4, 0]],float)        
 
-        flow_bgr = self.flow_map_to_bgr(flow)
-        return flow, flow_bgr
+        U = np.zeros([self.prev.shape[0],self.prev.shape[1]])
+        V = np.zeros([self.prev.shape[0],self.prev.shape[1]])       
+
+	    # iterate to refine U,V
+        for i in range(self.MAX_ITERS):
+            # Laplacian ==> f-fav 
+            Un_av = cv.filter2D(U,-1, LapKernel) + U
+            Vn_av = cv.filter2D(V,-1, LapKernel) + V
+
+            derivatives = (self.Ix*Un_av + self.Iy*Vn_av + self.It) / (self.ALPHA**2 + self.Ix**2 + self.Iy**2)
+
+            U = Un_av - self.Ix * derivatives
+            V = Vn_av - self.Iy * derivatives
+            
+            if checkL2(U, Un_av, V, Vn_av):
+                break
+
+        flow_bgr = self.flow_map_to_bgr((U, V))
+        return (U, V), flow_bgr
 
     #***********************************************************************************
     #calculate the angular error here
@@ -86,6 +107,23 @@ class OpticalFlow:
 
         return aae, aae_per_point
 
+def showImg(img, name="Image"):
+    cv.imshow(name,img) 
+    cv.waitKey(0) 
+    cv.destroyAllWindows()
+
+def checkL2(U, Uav, V, Vav):
+    row = U.shape[0]
+    col = U.shape[1]
+
+    error = 0
+    for i in range(row):
+        for j in range(col):
+            error += (abs(U[i][j]-Uav[i][j]) + abs(V[i][j]-Vav[i][j]))
+            if error > 0.002:
+                return False
+    return True
+    
 
 if __name__ == "__main__":
 
@@ -106,34 +144,50 @@ if __name__ == "__main__":
     for (i, (frame_filename, gt_filemane)) in enumerate(zip(data_list, gt_list)):
         groundtruth_flow = load_FLO_file(gt_filemane)
         img = cv.cvtColor(cv.imread(frame_filename), cv.COLOR_BGR2GRAY)
+        
         if not Op.next_frame(img):
             continue
 
-        flow_lucas_kanade, flow_lucas_kanade_bgr = Op.Lucas_Kanade_flow()
-        aae_lucas_kanade, aae_lucas_kanade_per_point = Op.calculate_angular_error(flow_lucas_kanade, groundtruth_flow)
-        print('Average Angular error for Luacas-Kanade Optical Flow: %.4f' %(aae_lucas_kanade))
+        #flow_lucas_kanade, flow_lucas_kanade_bgr = Op.Lucas_Kanade_flow()
+        #aae_lucas_kanade, aae_lucas_kanade_per_point = Op.calculate_angular_error(flow_lucas_kanade, groundtruth_flow)
+        #print('Average Angular error for Luacas-Kanade Optical Flow: %.4f' %(aae_lucas_kanade))
 
         flow_horn_schunck, flow_horn_schunck_bgr = Op.Horn_Schunck_flow()
-        aae_horn_schunk, aae_horn_schunk_per_point = Op.calculate_angular_error(flow_horn_schunck, groundtruth_flow)        
-        print('Average Angular error for Horn-Schunck Optical Flow: %.4f' %(aae_horn_schunk))   
+        #aae_horn_schunk, aae_horn_schunk_per_point = Op.calculate_angular_error(flow_horn_schunck, groundtruth_flow)        
+        #print('Average Angular error for Horn-Schunck Optical Flow: %.4f' %(aae_horn_schunk))   
 
-        flow_bgr_gt = Op.flow_map_to_bgr(groundtruth_flow)
+        #flow_bgr_gt = Op.flow_map_to_bgr(groundtruth_flow)
 
         fig = plt.figure(figsize=(img.shape))
-
+    
         # Display
-        fig.add_subplot(2, 3, 1)
-        plt.imshow(flow_bgr_gt)
-        fig.add_subplot(2, 3, 2)
-        plt.imshow(flow_lucas_kanade_bgr)
-        fig.add_subplot(2, 3, 3)
-        plt.imshow(aae_lucas_kanade_per_point)
-        fig.add_subplot(2, 3, 4)
-        plt.imshow(flow_bgr_gt)
-        fig.add_subplot(2, 3, 5)
-        plt.imshow(flow_horn_schunck_bgr)
-        fig.add_subplot(2, 3, 6)
-        plt.imshow(aae_horn_schunk_per_point)
-        plt.show()
+        #fig.add_subplot(2, 3, 1)
+        #plt.imshow(flow_bgr_gt)
+        #fig.add_subplot(2, 3, 2)
+        #plt.imshow(flow_lucas_kanade_bgr)
+        #fig.add_subplot(2, 3, 3)
+        #plt.imshow(aae_lucas_kanade_per_point)
+        #fig.add_subplot(2, 3, 4)
+        #plt.imshow(flow_bgr_gt)
+        #fig.add_subplot(2, 3, 5)
+        #plt.imshow(flow_horn_schunck_bgr)
+        #fig.add_subplot(2, 3, 6)
+        #plt.imshow(aae_horn_schunk_per_point)
+        #plt.show()
+        
+        # Just for testing
+        """U = flow_horn_schunck[0]
+        V = flow_horn_schunck[1]
+        A = U.copy()
+        for i in range(img.shape[0]):
+            for j in range(img.shape[1]):
+                A[i][j] = pow(U[i][j]*U[i][j] + V[i][j]*V[i][j],0.5)
+        ma = np.amax(A)
+        mi = np.amin(A)
 
+        for i in range(img.shape[0]):
+            for j in range(img.shape[1]):
+                img[i][j] = int(255 * (A[i][j]-mi)/(ma-mi) )
+        showImg(img)"""
+        
         print("*"*20)
