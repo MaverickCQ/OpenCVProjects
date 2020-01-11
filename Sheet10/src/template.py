0c2fc4dfaa6c9fd27f9b6f54523f06c177c1c529
unknown <s6anghos@uni-bonn.de>
2020-01-11 23:42:16 +0100

    task3,4

--- Sheet10/src/template.py
+++ Sheet10/src/template.py
@@ -21,9 +21,10 @@ for ptIdx in range(0, board_n):
 obj = np.vstack(obj)
 
 def showImg(img, name="Image"):
-    cv2.imshow(name,img) 
-    cv2.waitKey(0) 
-    cv2.destroyAllWindows()
+    cv2.imwrite(name,img)
+    #cv2.imshow(name,img) 
+    #cv2.waitKey(0) 
+    #cv2.destroyAllWindows()
 
 def task1():
     #implement your solution
@@ -33,9 +34,9 @@ def task1():
 
     objp = np.zeros((board_w * board_h, 3), np.float32)
     objp[:, :2] = np.mgrid[0:board_w, 0:board_h].T.reshape(-1, 2)
-    
+    i = 0
     for img_file in images_files_list:
-        print(img_file)
+        #print(img_file)
         img = cv2.imread(img_file)
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -51,7 +52,9 @@ def task1():
         else:
             print('No chessboard')
 
-        showImg(img, img_file)
+        showImg(img, str(i)+".png")
+        i+=1
+        #cv2.imwrite(str(i)+".png",img)
     return imagePoints, objectPoints
 
 def task2(imagePoints, objectPoints):
@@ -59,19 +62,54 @@ def task2(imagePoints, objectPoints):
     img = cv2.imread(images_files_list[0])
     img_size = (img.shape[0], img.shape[1])
     ret, CM, D, rvecs, tvecs = cv2.calibrateCamera(objectPoints, imagePoints, img_size, None, None)
-    
+    print("CM: ", CM)
+    print("D:", D)
+    print("rotation :", rvecs)
+    print("translation:", tvecs)
     return CM, D, rvecs, tvecs
 
-def task3(imagePoints, objectPoints, CM, D, rvecs, tvecs):
+def task3(imagePoints, objectPoints, CM, D, rvecs, tvecs):    
     #implement your solution
-    pass
-
+    #print("task3")
+    #newImagePoints = np.vstack(imagePoints)
+    #print(newImagePoints.shape)
+    step1 = 0
+    step2 = 0
+    k = 0 
+    i=0
+    subpix_criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.01)
+    for img_file in images_files_list:
+        img = cv2.imread(img_file)
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        ret, corners = cv2.findChessboardCorners(img_gray, board_size, None)
+        newCorners = cv2.cornerSubPix(img_gray, corners, (3, 3), (-1, -1), subpix_criteria)
+        corner_a = np.vstack(corners)
+        newCorner_a = np.vstack(newCorners)
+        step1 += np.sum(corner_a[:,0] - newCorner_a[:,0])
+        step2 += np.sum(corner_a[:,1] - newCorner_a[:,1])
+        k = corner_a.shape[0]
+    #print (step1)    
+    ex = step1 / (NUM_IMAGES * k)
+    ey = step2 / (NUM_IMAGES * k)
+    print ("Ex , Ey", ex, ey)
+    
 def task4(CM, D):
     #implement your solution
-    pass
+    print("task4")
+    i = 0
+    NCM, roi = cv2.getOptimalNewCameraMatrix(CM, D, (board_w, board_h), 1, (board_w, board_h))
+    for img_file in images_files_list:
+        img = cv2.imread(img_file)
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        dst = cv2.undistort(img_gray, CM, D, None, NCM)
+        showImg((np.abs(img_gray,dst)), "task4_"+str(i)+".png")
+        i+=1
+        print (np.sum(np.abs(img_gray,dst)))
+            
 
 def task5(CM, rvecs, tvecs):
     #implement your solution
+    
     pass
 
 def main():
@@ -84,13 +122,13 @@ def main():
     
     imagePoints, objectPoints = task1() #Calling Task 1
         
-    CM, D, rvecs, tvecs = task2(imagePoints, objectPoints) #Calling Task 2
+    CM, D, rvecs, tvecs = task2(imagePoints, objectPoints) #Calling Task 2   
 
     task3(imagePoints, objectPoints, CM, D, rvecs, tvecs)  # Calling Task 3
 
-    #task4(CM, D) # Calling Task 4
+    task4(CM, D) # Calling Task 4
 
-    #task5(CM, rvecs, tvecs) # Calling Task 5
+    task5(CM, rvecs, tvecs) # Calling Task 5
     
     print("FINISH!")
 
